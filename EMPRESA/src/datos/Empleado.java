07190d2bfc8e69fb7e38a6c95e518060d3cb9390
admin <admin@IC04.cesbur>
2014-04-01 11:47:14 +0200

    Creacion del metodo comparableTo, que nos compara los numeros de la SS ademas de crear el metodo de obtencion getNumeroSS.

--- EMPRESA/src/datos/Empleado.java
+++ EMPRESA/src/datos/Empleado.java
@@ -1,41 +1,60 @@
 package datos;
 
 
-public abstract class Empleado {
-	
-	//Atributos
+public abstract class Empleado  implements Comparable<Empleado>{
+
+	// Atributos
 	private String nombre;
 	private String apellidos;
 	private String numeroSS;
-	
-	//Constructores
-	public Empleado(String nombre, String apellidos, String numeroSS){
-		
-		this.nombre=nombre;
-		this.apellidos=apellidos;
-		this.numeroSS=numeroSS;
-	}
-	public Empleado(){}
 
-	//Métodos para dar valores a los atributos
+	// Constructores
+	public Empleado(String nombre, String apellidos, String numeroSS) {
+
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.numeroSS = numeroSS;
+	}
+
+	public Empleado() {
+	}
+
+	// Métodos para dar valores a los atributos
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
-	
 
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
 
-
 	public void setNumeroSS(String numeroSS) {
 		this.numeroSS = numeroSS;
 	}
-	
-	//Método para obtener una cadena de caracteres con los valores de los atributos
-	public  String  toString(){
-		return nombre + "  "+ apellidos + "  " + numeroSS;
+
+	// Metodo que nos devuelve el numero de la SS
+	public String getNumeroSS() {
+		return numeroSS;
 	}
 
+	// Método para obtener una cadena de caracteres con los valores de los
+	// atributos
+	public String toString() {
+		return nombre + "  " + apellidos + "  " + numeroSS;
+	}
+
+	// Metodo que nos compara los empleados por el numero de la SS.
+	public void compareTo(Empleado emple) {
+		int valor = 0;
+		if (numeroSS == emple.getNumeroSS()) {
+			valor = 0;
+		}
+		if (numeroSS > emple.getNumeroSS()) {
+			valor = 1;
+		}
+		if (numeroSS < emple.getNumeroSS()) {
+			valor = -1;
+		}
+		return valor;
+	}// Fin del metodo compareTo
 }
