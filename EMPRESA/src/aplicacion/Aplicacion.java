fd0c903a2b4da9d0fa4b1224a5d2f95a9ec0debc
Rubén <admin@IC01.cesbur>
2014-04-08 09:47:30 +0200

    Añadir submenu de añadir empleados.

--- EMPRESA/src/aplicacion/Aplicacion.java
+++ EMPRESA/src/aplicacion/Aplicacion.java
@@ -2,8 +2,14 @@ package aplicacion;
 
 import javax.swing.JOptionPane;
 
+import datos.Empleado;
+import datos.EmpleadoAsalariado;
+import datos.EmpleadoBaseMasComision;
+import datos.EmpleadoPorComision;
+import datos.EmpleadoPorHoras;
 import datos.Empresa;
 import menus.CrearMenus;
+
 
 public class Aplicacion {
 	
@@ -20,15 +26,53 @@ public class Aplicacion {
 		//Menu
 		int opcion;
 		String mensaje="1.- Añadir empleado\n2.- Borrar empleado.\n3.- Modificar precio hora\n"
-				+ "4.- Modificar tasa comisiones\5.- Modificar salario semanal\n6.- Listar";
+				+ "4.- Modificar tasa comisiones\n5.- Modificar salario semanal\n6.- Listar\n7.- Salir";
 		
 		do {
-			opcion=CrearMenus.crearMenu(mensaje, "Gestionar Empresa",6 );
-		} while (opcion!=6);
+			opcion=CrearMenus.crearMenu(mensaje, "Gestionar Empresa",7 );
+			switch (opcion) {
+			case 1:
+				añadirEmpleado();
+				break;
+
+			default:
+				break;
+			}
+		} while (opcion!=7);
 		
 	}
-	
-	
+	void añadirEmpleado(){
+		int opcion;
+		String mensaje="1.-Añadir empleado Asalariado\n2.-Añadir empleado Base mas Comisión\n3.-Añadir empleado"
+				+ " por Comision\n4.-Añadir empleado por Horas\n5.-Volver al menu principal";
+		do {
+			opcion=CrearMenus.crearMenu(mensaje, "Selecciona Tipo Empleado",5 );
+			switch (opcion) {
+			case 1:
+				EmpleadoAsalariado empleadoAsalariado = null;
+				darAltas(empleadoAsalariado);
+				break;
+			case 2:
+				EmpleadoBaseMasComision empleadoBaseMasComision = null;
+				darAltas(empleadoBaseMasComision);
+				break;
+			case 3:
+				EmpleadoPorComision empleadoPorComision = null;
+				darAltas(empleadoPorComision);
+				break;
+			case 4:
+				EmpleadoPorHoras empleadoPorHoras = null;
+				darAltas(empleadoPorHoras);
+				break;
+			case 5:
+				break;
+			}
+		} while (opcion!=5);
+			
+	}
+	void darAltas(Empleado e){
+		
+	}
 
 	public static void main(String[] args) {
 		new Aplicacion();
