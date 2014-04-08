c95f360a01f1ce9cc13c57729a7e31743abbcfb9
admin <admin@IC11.cesbur>
2014-04-08 10:12:02 +0200

    Añadimos el método borrarEmpleado en Aplicacion.java

--- EMPRESA/src/aplicacion/Aplicacion.java
+++ EMPRESA/src/aplicacion/Aplicacion.java
@@ -1,8 +1,15 @@
 package aplicacion;
 
+import java.util.Iterator;
+
 import javax.swing.JOptionPane;
 
+import datos.Empleado;
+import datos.EmpleadoAsalariado;
+import datos.EmpleadoPorComision;
+import datos.EmpleadoPorHoras;
 import datos.Empresa;
+import entradadatos.EntradaGraficaDatos;
 import menus.CrearMenus;
 
 public class Aplicacion {
@@ -58,10 +65,37 @@ public class Aplicacion {
 		
 	}
 	void borrarEmpleado(){
+		String numero = JOptionPane.showInputDialog("Número de la SS del empleado a borrar");
+		Empleado emple;
+		Iterator <Empleado> apuntador = empresa.getEmpleados().iterator();
+		while(apuntador.hasNext()) {
+			emple=apuntador.next();
+			if (numero == emple.getNumeroSS()){
+				empresa.bajaEmpleado(emple);
+			}
+		}
 		
 	}
 	void modificarSalarios(){
-		
+		String numero = JOptionPane.showInputDialog("Número de la SS del empleado a modificar");
+		Empleado emple;
+		double nuevoSueldo;
+		Iterator <Empleado> apuntador = empresa.getEmpleados().iterator();
+		while(apuntador.hasNext()) {
+			emple=apuntador.next();
+			if (numero == emple.getNumeroSS()){
+				if (emple instanceof EmpleadoAsalariado){
+					nuevoSueldo = EntradaGraficaDatos.pedirDouble("Nuevo salario semanal");
+					((EmpleadoAsalariado) emple).setSalarioSemanal(nuevoSueldo);
+				}else if (emple instanceof EmpleadoPorComision){
+					nuevoSueldo = EntradaGraficaDatos.pedirDouble("Nueva tasa comisiones");
+					((EmpleadoPorComision) emple).setTasaComisiones(nuevoSueldo);
+				} else if (emple instanceof EmpleadoPorHoras){
+					nuevoSueldo = EntradaGraficaDatos.pedirDouble("Nuevo precio por hora");
+					((EmpleadoPorHoras) emple).setPrecioHora(nuevoSueldo);
+				}
+			}
+		}
 	}
 	void listarEmpleados(){
 		
